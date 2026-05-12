0033ef97d90e088935e27dd81e18b7c277e34120
SviatoslaKondratiuk <ipz241_ksr@student.ztu.edu.ua>
2026-05-12 18:16:02 +0300

    refactor dip mainwindow dependencies

--- SWCPaint.Wpf/App.xaml.cs
+++ SWCPaint.Wpf/App.xaml.cs
@@ -1,14 +1,44 @@
-﻿using System.Configuration;
-using System.Data;
+using System.IO;
 using System.Windows;
+using SWCPaint.Core.Interfaces;
+using SWCPaint.Core.Interfaces.Persistence;
+using SWCPaint.Core.Interfaces.Serialization;
+using SWCPaint.Infrastructure.Persistence;
+using SWCPaint.Infrastructure.Serialization;
+using SWCPaint.Infrastructure.Services;
+using SWCPaint.Wpf.Services;
+using SWCPaint.Wpf.ViewModels;
 
-namespace SWCPaint
+namespace SWCPaint.Wpf
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config", "tools.json");
+
+            IDialogService dialogService = new WpfDialogService();
+            IFileManager fileManager = new PhysicalFileManager();
+            IProjectSerializer projectSerializer = new JsonProjectSerializer();
+            IImageExporter imageExporter = new WpfImageExporter();
+            IToolConfigurationService toolConfigurationService = new JsonToolConfigurationService(configPath, fileManager);
+
+            var viewModel = new MainViewModel(
+                toolConfigurationService,
+                dialogService,
+                fileManager,
+                projectSerializer,
+                imageExporter,
+            );
+
+            MainWindow mainWindow = new MainWindow(viewModel); 
+
+            mainWindow.Show();
+        }
+    }
 }
